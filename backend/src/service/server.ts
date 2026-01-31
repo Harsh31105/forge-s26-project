7a0f6320ceabaa260f113bf1ba445dc28f023380
Harsh31105 <harshnirajsingh@gmail.com>
2026-01-31 01:46:13 -0500

    feat: Server.ts completed

--- backend/src/service/server.ts
+++ backend/src/service/server.ts
@@ -6,6 +6,12 @@ import { config } from "../config/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import {SampleHandler} from "./handler/sample";
 import {sampleRoutes} from "./handler/sample/routes";
+import morgan from "morgan";
+import compression from "compression";
+import favicon from "serve-favicon";
+import path from "path";
+import cors from "cors";
+import {errorHandler} from "../errs/httpError";
 
 class App {
     public server: Express;
@@ -16,10 +22,28 @@ class App {
         this.repo = repo;
 
         this.server.use(express.json());
-
-        registerRoutes(this.server, this.repo);
+        this.server.use(express.urlencoded({ extended: true }));
+        this.server.use(morgan("dev"));
+        this.server.use(compression());
+        this.server.use(favicon(path.join(process.cwd(), "public", "favicon.io")));
+        this.server.use(cors({
+            origin: [
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:8080",
+                "http://127.0.0.1:8080",
+                "http://127.0.0.1:3000",
+            ],
+            methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+            allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization"],
+            credentials: true,
+            exposedHeaders: ["Content-Length", "X-Request-ID"],
+        }));
+        this.server.use(errorHandler);
 
         this.server.get("/health", (_req, res) => res.sendStatus(200));
+
+        registerRoutes(this.server, this.repo);
 
         this.server.use((_req, res) => res.status(404).json({ error: "Route not found" }));
     }
