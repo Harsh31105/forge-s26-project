22c726e44f2431ab519efae349d34b0b1f9dde44
Harsh31105 <harshnirajsingh@gmail.com>
2026-01-31 01:16:15 -0500

    feat: Server.

--- backend/cmd/main.ts
+++ backend/cmd/main.ts
@@ -1,20 +1,5 @@
-import process from "process";
-import { config, validateConfig } from "../src/config/config";
 import { initApp } from "../src/service/server";
+import { config } from "config/config";
 
-async function main() {
-    try {
-        // TODO: Write after initApp() in server.ts.
-        // validateConfig();
-        //
-        // const app = await initApp();
-        //
-        // const shutdown = async (signal: string) => {
-        //     console.info(`\nReceived ${signal}. Shutting down forgefully...`);
-        // }
-        //
-        // try {
-        //     if (app.server?.close)
-        // }
-    }
-}
+const app = initApp();
+app.listen(config.application.port);
--- backend/src/service/server.ts
+++ backend/src/service/server.ts
@@ -1,11 +1,49 @@
-import { Express } from "express";
+import express, { Express } from "express";
 import { Repository } from "../storage/storage";
+import {Pool} from "pg";
+import {configurePool, getConnectionString} from "../config/db";
+import { config } from "../config/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import {SampleHandler} from "./handler/sample";
+import {sampleRoutes} from "./handler/sample/routes";
 
-export class App {
+class App {
     public server: Express;
     public repo: Repository;
 
     constructor(repo: Repository) {
-        this.server = initApp(repo);
+        this.server = express();
+        this.repo = repo;
+
+        this.server.use(express.json());
+
+        registerRoutes(this.server, this.repo);
+
+        this.server.get("/health", (_req, res) => res.sendStatus(200));
+
+        this.server.use((_req, res) => res.status(404).json({ error: "Route not found" }));
     }
+
+    listen(port: string) {
+        this.server.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}`);
+        });
+    }
+}
+
+function initApp(): App {
+    const pool = new Pool({
+        connectionString: getConnectionString(config.db),
+    })
+    configurePool(pool, config.db);
+
+    const db = drizzle(pool);
+    const repo = new Repository(pool, db);
+
+    return new App(repo);
+}
+
+function registerRoutes(app: Express, repo: Repository) {
+    const sampleHandler = new SampleHandler(repo.samples);
+    app.use("/api/v1/samples", sampleRoutes(sampleHandler));
 }
