ac8a43ea05453acec83929e0b9f164f8caf54b16
TimPineda <pineda.t@northeastern.edu>
2026-03-04 17:18:30 -0500

    Set up configuration for google OAuth and installed dependencies for google api

---
+++ backend/src/config/google.ts
@@ -0,0 +1,13 @@
+import process from "process";
+
+export interface Google {
+    clientId: string;
+    clientSecret: string;
+    redirectURI: string;
+}
+
+export const googleConfig: Google = {
+    clientId : process.env.GOOGLE_CLIENT_ID!,
+    clientSecret : process.env.GOOGLE_CLIENT_SECRET!,
+    redirectURI : process.env.GOOGLE_REDIRECT_URI!
+}; 
