0d877047b289bb340196190effc04ca136cddbfc
chinychin99 <biaktchin8@gmail.com>
2026-03-14 07:28:10 -0400

    added models/trace.ts

---
+++ backend/src/models/trace.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export interface Trace {
+    id: string;
+    courseId?: string; // optional
+    professorId?: string; // optional
+    departmentId?: string; // optional
+    action: string;
+    timestamp: Date;
+}
+
+export const TracePostInputSchema = z.object({
+    courseId: z.string().optional(),
+    professorId: z.string().optional(),
+    departmentId: z.string().optional(),
+    action: z.string().min(1, "Action cannot be empty")
+        .refine((s) => s === s.trim(), "Action cannot have leading  spaces"),
+    timestamp: z.preprocess((val) => val ? new Date(val as string) : undefined,
+        z.date())
+});
+export type TracePostInputType = z.infer<typeof TracePostInputSchema>;
+
+export const TracePatchInputSchema = TracePostInputSchema.partial();
+export type TracePatchInputType = z.infer<typeof TracePatchInputSchema>;
