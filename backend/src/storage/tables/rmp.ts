617f05f72c06ab1e7902a4c901511bad3bfe5514
salenakha <salenakha7@gmail.com>
2026-03-26 17:20:11 -0400

    drizzle update

--- backend/src/storage/tables/rmp.ts
+++ backend/src/storage/tables/rmp.ts
@@ -1,5 +1,6 @@
-import { pgTable, serial, uuid, decimal, integer, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, uuid, decimal, integer, timestamp, check } from "drizzle-orm/pg-core";
 import { professor } from "./professor";
+import { sql } from "drizzle-orm";
 
 export const rmp = pgTable("rmp", {
     id: serial("id").primaryKey(),
@@ -9,4 +10,8 @@ export const rmp = pgTable("rmp", {
     avgDifficulty: decimal("avg_difficulty", { precision: 3, scale: 2 }).notNull(),
     createdAt: timestamp("created_at", { mode: "date", withTimezone: true }).defaultNow().notNull(),
     updatedAt: timestamp("updated_at", { mode: "date", withTimezone: true }).defaultNow().notNull(),
-});
+}, (table) => [
+    check("rating_avg_check", sql`${table.ratingAvg} >= 1 AND ${table.ratingAvg} <= 5`),
+    check("rating_wta_check", sql`${table.ratingWta} BETWEEN 0 AND 100`),
+    check("avg_difficulty_check", sql`${table.avgDifficulty} >= 1 AND ${table.avgDifficulty} <= 5`),
+]);
