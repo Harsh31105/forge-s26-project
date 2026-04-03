1793858173fd46ed38f332a874a8a58c5f87a654
Harsh31105 <harshnirajsingh@gmail.com>
2026-04-03 01:48:50 -0400

    feat: Handler

---
+++ backend/src/service/handler/trace/index.ts
@@ -0,0 +1,24 @@
+import {PaginationSchema} from "../../../utils/pagination";
+import {BadRequest, mapDBError} from "../../../errs/httpError";
+import {Trace} from "../../../models/trace";
+import { Request, Response } from "express";
+
+export class TraceHandler {
+    constructor(private readonly repo: TraceRepository) {}
+
+    async handleGet(req: Request, res: Response): Promise<void> {
+        const result = PaginationSchema.safeParse(req.query);
+        if (!result.success) throw BadRequest("Invalid pagination parameters");
+        const pagination = result.data;
+
+        let traces: Trace[]
+        try {
+            traces = await this.repo.getTraces(pagination);
+        } catch (err) {
+            console.error("DB Error in get traces: ", err);
+            throw mapDBError(err, "failed to retrieve traces");
+        }
+
+        res.status(200).json(traces);
+    }
+}
