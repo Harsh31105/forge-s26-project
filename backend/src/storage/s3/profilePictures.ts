6c8e0f5fe3f16dc7a49c0064cbd66ef577159cf4
Harsh31105 <harshnirajsingh@gmail.com>
2026-04-20 16:33:33 -0400

    test: auth test fix

--- backend/src/storage/s3/profilePictures.ts
+++ backend/src/storage/s3/profilePictures.ts
@@ -7,7 +7,7 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import type { S3 as S3Config } from "../../config/s3";
-import { S3StorageError, mapS3Error } from "./traceDocuments";
+import { mapS3Error } from "./traceDocuments";
 
 const PRESIGNED_URL_EXPIRES_IN = 60 * 60; // 1 hour
 
