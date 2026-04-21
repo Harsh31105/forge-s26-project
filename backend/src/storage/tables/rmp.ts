739b9360581bf8429888587f95f2682f51fd0e2f
Harsh31105 <harshnirajsingh@gmail.com>
2026-04-21 18:36:40 -0400

    feat: fixing testutils and trace table representation

--- backend/src/storage/tables/rmp.ts
+++ backend/src/storage/tables/rmp.ts
@@ -7,7 +7,7 @@ export const rmp = pgTable("rmp", {
     professorId: uuid("professor_id").notNull().references(() => professor.id, { onDelete: "cascade" }),
     ratingAvg: decimal("rating_avg", { precision: 3, scale: 2 }),
     ratingWta: integer("rating_wta"),
-    avgDifficulty: decimal("avg_difficulty", { precision: 3, scale: 2 }).notNull(),
+    avgDifficulty: decimal("avg_difficulty", { precision: 3, scale: 2 }),
     createdAt: timestamp("created_at", { mode: "date", withTimezone: true }).defaultNow().notNull(),
     updatedAt: timestamp("updated_at", { mode: "date", withTimezone: true }).defaultNow().notNull(),
 }, (table) => [
