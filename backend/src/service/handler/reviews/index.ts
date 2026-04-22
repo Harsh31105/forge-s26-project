ea5e3bf8c0e117c7ab49af858a0b63dd56f1f7da
Salena <140116192+salenakha@users.noreply.github.com>
2026-04-22 00:01:57 -0400

    [BE/FE] Compare Page (#102)
    * compare component draft
    
    * compare page
    
    * made course card + updated page
    
    * fix build issue
    
    * dependencies
    
    * merge conflict
    
    * feat: build fix
    
    * fix error
    
    * hours/week fix
    
    * cont.
    
    * chore: it's using router.back() and we're not directing from course anymore..
    
    * removed upper limit
    
    * uses search icon instead + get rid of span
    
    * tag
    
    * limit git add .
    
    * try course pagination instead
    
    * Revert "try course pagination instead"
    
    This reverts commit a7ce18e2956ce9db92ae716476f10e50a33c2f21.
    
    * fix bug with pagination
    
    * feat: fixes
    
    ---------
    
    Co-authored-by: Harsh31105 <harshnirajsingh@gmail.com>

--- backend/src/service/handler/reviews/index.ts
+++ backend/src/service/handler/reviews/index.ts
@@ -25,6 +25,8 @@ export class ReviewHandler {
     const pagination = PaginationSchema.safeParse(req.query);
     if (!pagination.success) throw BadRequest("invalid pagination parameters");
 
+    if (pagination.data.limit > 100) throw BadRequest("Pagination limit must be between 1 and 100");
+
     let reviews: Review[];
 
     try {
